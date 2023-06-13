49856f6c37afd0dc4699e8772da169cb30143d7b
Kayky Cordeiro <90627779+kaykyc@users.noreply.github.com>
2023-06-12 22:19:30 -0300

    Implementação do FastAPI

---
+++ models/produto.py
@@ -0,0 +1,13 @@
+import ormar
+from sqlalchemy.sql.expression import table
+from config import database, metadata
+
+class Produto(ormar.Model):
+    class Meta:
+        metadata = metadata
+        database = database
+
+    cod: int = ormar.Integer(primary_key=True)
+    nome: str = ormar.String(max_length=50)
+    preco: str = ormar.String(max_length=6)
+    qnt: int = ormar.Integer(max_value=2)
